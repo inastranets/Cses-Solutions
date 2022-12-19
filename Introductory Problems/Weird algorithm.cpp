6ae569ac86cedfff40646fad38adec5432d08a9e
inastranets <90534213+inastranets@users.noreply.github.com>
2022-12-20 02:54:26 +0300

    Create Weird algorithm.cpp

---
+++ Introductory Problems/Weird algorithm.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+  ios_base::sync_with_stdio(0);
+  cin.tie(0);
+  
+  long long n; 
+  cin >> n;
+  while(true) {
+    cout << n << ' ';
+    if(n == 1)
+      break;
+    if(n & 1) // n is odd
+      n = n * 3 + 1;
+    else // n is even
+      n /= 2;
+  }
+  return 0;
+}
