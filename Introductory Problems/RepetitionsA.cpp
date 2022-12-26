8ec0c2b9b2d28e80c86b7c81144da147e615ea0a
Uchkunov Boburmirzo <90534213+inastranets@users.noreply.github.com>
2022-12-26 22:19:50 +0300

    Add files via upload

---
+++ Introductory Problems/RepetitionsA.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main () {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+
+	string s; 
+	int best = 0;
+	cin >> s;
+	int i = 0;
+	while(i < s.size()) {
+		int j = i;
+		int count = 0;
+		while(s[j] == s[j++ + 1])
+			++ count;
+		best = max(best, count);
+		i = j;
+	}
+	cout << best + 1;
+	return 0;	
+}
